a8bdbda6000e59635f0d7640ae558ea0a23e8af0
1e10Laptop <sample@bths.edu>
2022-09-30 10:56:15 -0400

    work

---
+++ src/Point.java
@@ -0,0 +1,21 @@
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public Point(int value) {
+        x = value;
+        y = value;
+    }
+    public Point() {
+        x = 0;
+        y = 0;
+    }
+
+}
+
+
+
